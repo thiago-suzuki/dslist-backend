4d5033a07ed1bb00d376ec99df56fe4e66671633
Thiago Ikenaga Suzuki <dev7.gbmlogistica@outlook.com>
2023-07-10 23:52:28 -0300

    Project created

---
+++ src/main/java/com/suzukidevsuperior/dslistbackendnew/DslistBackendNewApplication.java
@@ -0,0 +1,13 @@
+package com.suzukidevsuperior.dslistbackendnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suzukidevsuperior/dslistbackendnew/DslistBackendNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suzukidevsuperior.dslistbackendnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
